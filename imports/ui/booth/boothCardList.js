fa806860cff150319eca2597a48a9e7c81e85b4f
Michael <breadfit@gmail.com>
2021-03-12 17:52:34 +0900

    부스 리스트 업데이트

--- imports/ui/booth/boothCardList.js
+++ imports/ui/booth/boothCardList.js
@@ -5,6 +5,39 @@ import './boothCardList.html';
 Template.boothCardList.helpers({
     booths() {
         let boothsList = [
+
+            {
+                "_id" : "6HqRtbGNqaKxkh84B",
+                "idx" : 8,
+                "userId" : "XcA6umnBkRB6zcfMg",
+                "grade" : "diamond",
+                "company" : {
+                    "kor" : "써모 피셔 사이언티픽 코리아㈜",
+                    "eng" : "Thermo Fisher Scientific",
+                    "tel" : "02-2023-0600",
+                    "address" : "서울시 강남구 광평로 281, 수서오피스 빌딩 11층",
+                    "intro" : "'글로벌 질량분석 기업'",
+                    "thumb" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/thumb/ksev_thumb_08.png",
+                    "logo" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/icon/ksev_icon_08.png"
+                },
+                "manager" : {
+                    "name" : "서정우",
+                    "position" : "책임",
+                    "tel" : "02-3420-8611",
+                    "email" : "Joungwoo.seo@thermofisher.com"
+                },
+                "site" : {
+                    "homepage" : "www.thermofisher.com",
+                    "facebook" : "",
+                    "instagram" : "",
+                    "youtube" : "https://youtube.com/channel/UCTxqceNv1lRHpvZSYnEnCug",
+                    "twitter" : "",
+                    "etc" : "https://blog.naver.com/thermofisherkorea"
+                },
+                "content" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/content/thermofisher_content.png",
+                "brochure" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/content/thermofisher_brochure.pdf",
+                "video" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/content/thermofisher_video.mp4"
+            },
             {
                 "_id" : "tyoCKzYZTCiECKrM7",
                 "idx" : 2,
@@ -42,7 +75,7 @@ Template.boothCardList.helpers({
                 "_id" : "AuAE7mXoPpQopqDzt",
                 "idx" : 4,
                 "userId" : "XcA6umnBkRB6zcfMg",
-                "grade" : "gold",
+                "grade" : "diamond",
                 "company" : {
                     "kor" : "브렉소젠㈜",
                     "eng" : "BREXOGEN",
@@ -74,7 +107,7 @@ Template.boothCardList.helpers({
                 "_id" : "B5TYLs9mHwb8x7G6B",
                 "idx" : 5,
                 "userId" : "XcA6umnBkRB6zcfMg",
-                "grade" : "gold",
+                "grade" : "diamond",
                 "company" : {
                     "kor" : "엑소코바이오",
                     "eng" : "EXOCOBIO",
@@ -113,7 +146,7 @@ Template.boothCardList.helpers({
                 "_id" : "eYoKDrBsR5xcZkGF3",
                 "idx" : 7,
                 "userId" : "XcA6umnBkRB6zcfMg",
-                "grade" : "gold",
+                "grade" : "diamond",
                 "company" : {
                     "kor" : "주식회사 엑소퍼트",
                     "eng" : "EXOPERT",
@@ -211,7 +244,7 @@ Template.boothCardList.helpers({
                 "_id" : "yzLJdTBg5rhN2fcC3",
                 "idx" : 6,
                 "userId" : "XcA6umnBkRB6zcfMg",
-                "grade" : "gold",
+                "grade" : "diamond",
                 "company" : {
                     "kor" : "(주)파나진",
                     "eng" : "PANAGENE",
@@ -237,39 +270,6 @@ Template.boothCardList.helpers({
                 },
                 "content" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/content/panagene_content.png",
                 "brochure" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/content/panagene_brochure.pdf"
-            },
-
-            {
-                "_id" : "6HqRtbGNqaKxkh84B",
-                "idx" : 8,
-                "userId" : "XcA6umnBkRB6zcfMg",
-                "grade" : "diamond",
-                "company" : {
-                    "kor" : "써모 피셔 사이언티픽 코리아㈜",
-                    "eng" : "Thermo Fisher Scientific",
-                    "tel" : "02-2023-0600",
-                    "address" : "서울시 강남구 광평로 281, 수서오피스 빌딩 11층",
-                    "intro" : "'글로벌 질량분석 기업'",
-                    "thumb" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/thumb/ksev_thumb_08.png",
-                    "logo" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/icon/ksev_icon_08.png"
-                },
-                "manager" : {
-                    "name" : "서정우",
-                    "position" : "책임",
-                    "tel" : "02-3420-8611",
-                    "email" : "Joungwoo.seo@thermofisher.com"
-                },
-                "site" : {
-                    "homepage" : "www.thermofisher.com",
-                    "facebook" : "",
-                    "instagram" : "",
-                    "youtube" : "https://youtube.com/channel/UCTxqceNv1lRHpvZSYnEnCug",
-                    "twitter" : "",
-                    "etc" : "https://blog.naver.com/thermofisherkorea"
-                },
-                "content" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/content/thermofisher_content.png",
-                "brochure" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/content/thermofisher_brochure.pdf",
-                "video" : "https://medience.s3.ap-northeast-2.amazonaws.com/ksev.2020/booth/content/thermofisher_video.mp4"
             }
         ];
 
